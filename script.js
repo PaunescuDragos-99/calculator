c60fe344205f5f5cad79222de723f8901b9ef6a1
PaunescuDragos-99 <dragospaunescu19@gmail.com>
2023-11-24 22:17:01 +0200

    added top info for last calcul and fixed some cases

--- script.js
+++ script.js
@@ -4,55 +4,81 @@ let lastSymbol = "";
 let lastCalled = "notEqual"
 let multiplyWasCalled = false;
 let decimalNumber = false;
+let inputZero = false;
 
-const screen = document.querySelector(".calculator-screen");
+const screenInfo = document.querySelector(".current-info");
+const pastInfo = document.querySelector(".past-info");
 
 function equation(symbol){
-    lastCalled = "notEqual";
+    if(lastCalled != "equal" && operatorBefore !=0){
+        if((lastSymbol == "multiply" && curentOperator == 0 || lastSymbol == "division" && curentOperator == 0) && inputZero == false){
+            curentOperator = 1;
+            equal();
+            console.log(curentOperator, lastSymbol,"1");
+        }else if(lastSymbol == "multiply-negat" && (curentOperator == 0 && inputZero == false)){
+            curentOperator = -1;
+            equal();
+            console.log(curentOperator, lastSymbol,"2")
+        }else{
+            equal();
+            console.log(curentOperator, lastSymbol,"3");
+
+        }
+    }
+
     if(operatorBefore == 0){
         operatorBefore += parseFloat(curentOperator);
+        pastInfo.textContent = `${operatorBefore}`;
     }
-    if(multiplyWasCalled == true && symbol == "minus" && lastCalled == "Equal"){
-        lastSymbol = "multiply-negat";
-        screen.textContent = `-`;
-        curentOperator = "0";
-    }else if(symbol == "plus"){
+    if(symbol == "plus"){
         lastSymbol = "plus";
-        screen.textContent = `+`;
+        screenInfo.textContent = `+`;
         curentOperator = "0";
         multiplyWasCalled = false;
+        pastInfo.textContent = `${operatorBefore} + `;
     }else if(symbol == "multiply"){
         lastSymbol = "multiply";
-        screen.textContent = `*`;
+        screenInfo.textContent = `*`;
         curentOperator = "0";
         multiplyWasCalled = true;
+        pastInfo.textContent = `${operatorBefore} * `;
     }else if(symbol == "minus"){
         lastSymbol = "minus";
-        screen.textContent = `-`;
+        screenInfo.textContent = `-`;
         curentOperator = "0";
+        pastInfo.textContent = `${operatorBefore} - `;
+        if(multiplyWasCalled == true && lastCalled == "equal"){
+            lastSymbol = "multiply-negat";
+            screenInfo.textContent = `-`;
+            pastInfo.textContent = `${operatorBefore} * (- ) `;
+        }
         multiplyWasCalled = false;
     }else if(symbol == "division"){
         lastSymbol = "division";
-        screen.textContent = `/`;
+        screenInfo.textContent = `/`;
         curentOperator = "0";
         multiplyWasCalled = false;
+        pastInfo.textContent = `${operatorBefore} / `;
+
     }
+    lastCalled = "notEqual";
 }
 
 function clean(){
     curentOperator = "0";
     operatorBefore = 0;
     lastCalled = "notEqual"
-    screen.textContent = `${operatorBefore}`;
+    screenInfo.textContent = `${operatorBefore}`;
+    pastInfo.textContent = ` `;
 }
 
 function negat(){
     if(curentOperator != 0){
         curentOperator *= (-1);
-        screen.textContent = `${curentOperator}`;
+        screenInfo.textContent = `${curentOperator}`;
     }else{
         operatorBefore *= (-1);
-        screen.textContent = `${operatorBefore}`;
+        screenInfo.textContent = `${operatorBefore}`;
     }
 }
 
@@ -60,17 +86,17 @@ function deleteValue(){
     if(curentOperator != 0){
         if(curentOperator < 0){
             curentOperator = Math.ceil(curentOperator / 10);
-            screen.textContent = `${curentOperator}`;
+            screenInfo.textContent = `${curentOperator}`;
         }else{
             curentOperator = Math.floor(curentOperator / 10);
-            screen.textContent = `${curentOperator}`;
+            screenInfo.textContent = `${curentOperator}`;
         }
     }else if(operatorBefore < 0){
         operatorBefore = Math.ceil(operatorBefore / 10);
-        screen.textContent = `${operatorBefore}`;
+        screenInfo.textContent = `${operatorBefore}`;
     }else{
         operatorBefore = Math.floor(operatorBefore / 10);
-        screen.textContent = `${operatorBefore}`;
+        screenInfo.textContent = `${operatorBefore}`;
     }
 }
 
@@ -79,24 +105,34 @@ function equal(){
     if(lastSymbol == "plus"){
         operatorBefore += parseFloat(curentOperator);
         curentOperator = "0";
-        screen.textContent = `${operatorBefore}`;
+        screenInfo.textContent = `${operatorBefore}`;
+        pastInfo.textContent = `${operatorBefore}`;
         console.log(operatorBefore);
     }else if(lastSymbol == "minus"){
         operatorBefore -= parseFloat(curentOperator);
         curentOperator = "0";
-        screen.textContent = `${operatorBefore}`;
+        pastInfo.textContent = `${operatorBefore}`;
+        screenInfo.textContent = `${operatorBefore}`;
     }else if (lastSymbol == "multiply"){
         operatorBefore *= parseFloat(curentOperator);
         curentOperator = "0";
-        screen.textContent = `${operatorBefore}`;
+        pastInfo.textContent = `${operatorBefore}`;
+        screenInfo.textContent = `${operatorBefore}`;
     }else if (lastSymbol == "division"){
+        if(curentOperator == 0){
+            screenInfo.textContent = 'ERROR!';
+        }else{
         operatorBefore /= parseFloat(curentOperator);
         curentOperator = "0";
-        screen.textContent = `${operatorBefore}`;
+        pastInfo.textContent = `${operatorBefore}`;
+        screenInfo.textContent = `${operatorBefore}`;
+        }
     }else if (lastSymbol = "multiply-negat"){
         operatorBefore *= -parseFloat(curentOperator);
         curentOperator = "0";
-        screen.textContent = `${operatorBefore}`;
+        pastInfo.textContent = `${operatorBefore}`;
+        screenInfo.textContent = `${operatorBefore}`;
+        multiplyWasCalled = false;
     }
 }
 
@@ -104,9 +140,7 @@ function addDecimal(bool){
     if(bool == true && decimalNumber == false){
         curentOperator += '.';
         decimalNumber = true;
-        console.log(decimalNumber);
-        console.log("number", curentOperator);
-        screen.textContent = `${curentOperator}`;
+        screenInfo.textContent = `${curentOperator}`;
     }
 }
 
@@ -118,6 +152,13 @@ function number(value){
         curentOperator = "";
         decimalNumber = false;
     }
-    curentOperator += value;
-    screen.textContent = `${curentOperator}`;
+    if(value == "0"){
+        curentOperator += value;
+        screenInfo.textContent = `${curentOperator}`;
+        inputZero = true;
+    }else{
+        curentOperator += value;
+        screenInfo.textContent = `${curentOperator}`;
+        inputZero = false;
+    }    
 }
